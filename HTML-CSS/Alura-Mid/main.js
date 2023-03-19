069e07d9a11d0d0460df3226bd231f7a6aef45f3
Brenno Alves <brennoao@outlook.com>
2023-03-19 18:02:49 -0300

    Curso-1_FormaçãoJavaScript

--- HTML-CSS/Alura-Mid/main.js
+++ HTML-CSS/Alura-Mid/main.js
@@ -1,20 +1,25 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play()
+function tocaSom (seletorAudio) {
+   const elemento = document.querySelector(seletorAudio)
+
+   if (elemento != null && elemento.localName == 'audio') {
+        elemento.play()
+   } else {
+    console.log('Elemento não encontrado ou seletor inválido')
+   }
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-let contador = 0
+/*let contador = 0
 
 //Estrutura de repetição while
 
 // while = Enquanto 
-/* while (contador < listaDeTeclas.length ) {
+while (contador < listaDeTeclas.length ) {
     const tecla = listaDeTeclas[contador]
     const instrumento = tecla.classList[1]
 
-    // Template string
-    const idAudio = `#som_${instrumento}`
+    const idAudio = `#som_${instrumento}` // Template string
 
     //console.log(idAudio)
 
@@ -27,3 +32,25 @@ let contador = 0
 
     //console.log(contador)
 } */
+
+
+// for=Para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1]
+    const idAudio = `#som_${instrumento}` // Template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio)
+    }
+
+    tecla.oncKeydown = function (evento) {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onKeyup = function () {
+        tecla.classList.remove('ativa')
+    }
+}
